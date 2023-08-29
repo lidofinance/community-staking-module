dac4e3e297b66e05739386698e9f3e370c875493
madlabman <10616301+madlabman@users.noreply.github.com>
2023-08-29 10:07:11 +0200

    chore: forge init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,37 @@
+import fs from "fs";
+import "hardhat-preprocessor";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => line.trim().split("="));
+}
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.22",
+  paths: {
+    sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
+    cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
+  },
+  // This fully resolves paths for imports in the ./lib directory for Hardhat
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
 };
 
 export default config;
