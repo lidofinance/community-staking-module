c5fbc738e8babffd3a330098fa8feb4c40ad1d50
skhomuti <skhomuti@gmail.com>
2023-09-15 15:41:58 +0500

    feat: implement staking module summary

--- src/interfaces/ILido.sol
+++ src/interfaces/ILido.sol
@@ -10,4 +10,10 @@ import { IStETH } from "./IStETH.sol";
  */
 interface ILido is IStETH {
     function submit(address _referal) external payable returns (uint256);
+
+    function deposit(
+        uint256 _maxDepositsCount,
+        uint256 _stakingModuleId,
+        bytes calldata _depositCalldata
+    ) external;
 }
