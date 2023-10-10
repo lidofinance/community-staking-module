fe5b0da20bb93ec11c4b7b469c5ea2f183c2176b
skhomuti <skhomuti@gmail.com>
2023-10-10 18:54:14 +0500

    mapStorage -> self

--- src/lib/StringToUint256WithZeroMap.sol
+++ src/lib/StringToUint256WithZeroMap.sol
@@ -2,20 +2,20 @@ pragma solidity 0.8.21;
 
 library StringToUint256WithZeroMap {
 
-    function set(mapping(string => uint256) storage mapStorage, string memory key, uint256 value) internal {
-        mapStorage[key] = value + 1;
+    function set(mapping(string => uint256) storage self, string memory key, uint256 value) internal {
+        self[key] = value + 1;
     }
 
-    function get(mapping(string => uint256) storage mapStorage, string memory key) internal view returns (uint256) {
-        require(exists(mapStorage, key), "StringToUint256WithZeroMap: key does not exist");
-        return mapStorage[key] - 1;
+    function get(mapping(string => uint256) storage self, string memory key) internal view returns (uint256) {
+        require(exists(self, key), "StringToUint256WithZeroMap: key does not exist");
+        return self[key] - 1;
     }
 
-    function exists(mapping(string => uint256) storage mapStorage, string memory key) internal view returns (bool) {
-        return mapStorage[key] != 0;
+    function exists(mapping(string => uint256) storage self, string memory key) internal view returns (bool) {
+        return self[key] != 0;
     }
 
-    function remove(mapping(string => uint256) storage mapStorage, string memory key) internal {
-        delete mapStorage[key];
+    function remove(mapping(string => uint256) storage self, string memory key) internal {
+        delete self[key];
     }
 }
