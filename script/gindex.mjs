e0ae9870248905c3127e2aa33dcd39ef6d20e0aa
madlabman <10616301+madlabman@users.noreply.github.com>
2024-05-31 12:54:06 +0200

    refactor: extract pack function

--- script/gindex.mjs
+++ script/gindex.mjs
@@ -8,9 +8,10 @@ const BeaconBlock = ssz.deneb.BeaconBlock;
   const Withdrawals = BeaconBlock.getPathInfo(["body", "executionPayload", "withdrawals"]).type;
   const nav = BeaconState.getPathInfo(["latestExecutionPayloadHeader", "withdrawalsRoot"]);
 
-  const gI =
-    (concatGindices([nav.gindex, Withdrawals.getPropertyGindex(0)]) << 8n) |
-    BigInt(Math.log2(Withdrawals.limit));
+  const gI = pack(
+    concatGindices([nav.gindex, Withdrawals.getPropertyGindex(0)]),
+    Withdrawals.limit,
+  );
 
   console.log(
     "BeaconState.latest_execution_payload_header.withdrawals[0]:",
@@ -19,15 +20,25 @@ const BeaconBlock = ssz.deneb.BeaconBlock;
 }
 
 {
-  const gI =
-    (BeaconState.getPathInfo(["validators", 0]).gindex << 8n) |
-    BigInt(Math.log2(BeaconState.getPathInfo(["validators"]).type.limit));
+  const gI = pack(
+    BeaconState.getPathInfo(["validators", 0]).gindex,
+    BeaconState.getPathInfo(["validators"]).type.limit,
+  );
 
   console.log("BeaconState.validators[0]`: ", `0x${gI.toString(16)}`);
 }
 
 {
   const nav = BeaconState.getPathInfo(["historicalSummaries"]);
-  const gI = (nav.gindex << 8n) | 0n; // 0 because `historicalSummaries` is a bytes32 value.
+  const gI = pack(nav.gindex, 0); // 0 because `historicalSummaries` is a bytes32 value.
   console.log("BeaconState.historical_summaries`: ", `0x${gI.toString(16)}`);
+}
+
+// Analog of the GIndex.pack.
+// @param {bigint} gI
+// @param {number} limit
+// @return {bigint}
+function pack(gI, limit) {
+  const width = limit ? BigInt(Math.log2(limit)) : 0n;
+  return (gI << 8n) | width;
 }
