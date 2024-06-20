2376c3ebb0ae6e6a8b38448cdd7082f4e60e604f
Dmitry Gusakov <dmitry.gusakov@lido.fi>
2024-06-20 16:27:44 +0200

    Update src/CSModule.sol
    Co-authored-by: Vladimir Gorkavenko <32727352+vgorkavenko@users.noreply.github.com>

--- src/CSModule.sol
+++ src/CSModule.sol
@@ -1261,11 +1261,9 @@ contract CSModule is
                 // No need for `_updateDepositableValidatorsCount` call since we update the number directly.
                 // `keysCount` is min of `depositableValidatorsCount` and `depositsLeft`.
                 // @dev No need to safe cast due to internal logic
-                no.depositableValidatorsCount -= uint32(keysCount);
-                emit DepositableSigningKeysCountChanged(
-                    noId,
-                    no.depositableValidatorsCount
-                );
+                newCount = no.depositableValidatorsCount - uint32(keysCount);
+                no.depositableValidatorsCount = newCount
+                emit DepositableSigningKeysCountChanged(noId, newCount);
                 depositsLeft -= keysCount;
                 if (depositsLeft == 0) {
                     break;
