3caae173c1e501f1d4a5a2a07e6a549dae9de5e5
skhomuti <skhomuti@gmail.com>
2024-07-10 12:12:33 +0500

    fix: obtain deposit data for zero keys leads to OutOfGas if queue is not empty

--- src/CSModule.sol
+++ src/CSModule.sol
@@ -1192,6 +1192,8 @@ contract CSModule is
         returns (bytes memory publicKeys, bytes memory signatures)
     {
         (publicKeys, signatures) = SigningKeys.initKeysSigsBuf(depositsCount);
+        if (depositsCount == 0) return (publicKeys, signatures);
+
         uint256 depositsLeft = depositsCount;
         uint256 loadedKeysCount = 0;
 
