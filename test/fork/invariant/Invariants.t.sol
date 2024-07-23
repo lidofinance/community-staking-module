44cc6aa8b925939b62ef794fc63c681a3afe9fd8
skhomuti <skhomuti@gmail.com>
2024-07-23 10:39:23 +0500

    Add roles invariant tests

--- test/fork/invariant/Invariants.t.sol
+++ test/fork/invariant/Invariants.t.sol
@@ -142,6 +142,52 @@ contract CSModuleInvariants is InvariantsBase {
             );
         }
     }
+
+    function test_roles() public {
+        assertEq(
+            csm.getRoleMemberCount(csm.DEFAULT_ADMIN_ROLE()),
+            1,
+            "default admin"
+        );
+        assertEq(csm.getRoleMemberCount(csm.PAUSE_ROLE()), 1, "pause");
+        assertEq(
+            csm.getRoleMember(csm.PAUSE_ROLE(), 0),
+            address(gateSeal),
+            "pause address"
+        );
+        assertEq(csm.getRoleMemberCount(csm.RESUME_ROLE()), 0, "resume");
+        assertEq(
+            csm.getRoleMemberCount(csm.MODULE_MANAGER_ROLE()),
+            0,
+            "module manager"
+        );
+        assertEq(
+            csm.getRoleMemberCount(csm.STAKING_ROUTER_ROLE()),
+            1,
+            "staking router"
+        );
+        assertEq(
+            csm.getRoleMemberCount(
+                csm.REPORT_EL_REWARDS_STEALING_PENALTY_ROLE()
+            ),
+            1,
+            "report el rewards stealing penalty"
+        );
+        assertEq(
+            csm.getRoleMemberCount(
+                csm.SETTLE_EL_REWARDS_STEALING_PENALTY_ROLE()
+            ),
+            1,
+            "settle el rewards stealing penalty"
+        );
+        assertEq(csm.getRoleMemberCount(csm.VERIFIER_ROLE()), 1, "verifier");
+        assertEq(
+            csm.getRoleMember(csm.VERIFIER_ROLE(), 0),
+            address(verifier),
+            "verifier address"
+        );
+        assertEq(csm.getRoleMemberCount(csm.RECOVERER_ROLE()), 0, "recoverer");
+    }
 }
 
 contract CSAccountingInvariants is InvariantsBase {
@@ -171,6 +217,65 @@ contract CSAccountingInvariants is InvariantsBase {
             "assert allowance"
         );
     }
+
+    function test_roles() public {
+        assertEq(
+            accounting.getRoleMemberCount(accounting.DEFAULT_ADMIN_ROLE()),
+            1,
+            "default admin"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.PAUSE_ROLE()),
+            1,
+            "pause"
+        );
+        assertEq(
+            csm.getRoleMember(accounting.PAUSE_ROLE(), 0),
+            address(gateSeal),
+            "pause address"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.RESUME_ROLE()),
+            0,
+            "resume"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.ACCOUNTING_MANAGER_ROLE()),
+            0,
+            "accounting manager"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.MANAGE_BOND_CURVES_ROLE()),
+            0,
+            "manage bond curves"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.SET_BOND_CURVE_ROLE()),
+            2,
+            "set bond curve"
+        );
+        assertTrue(
+            accounting.hasRole(accounting.SET_BOND_CURVE_ROLE(), address(csm)),
+            "set bond curve csm"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.RESET_BOND_CURVE_ROLE()),
+            2,
+            "reset bond curve"
+        );
+        assertTrue(
+            accounting.hasRole(
+                accounting.RESET_BOND_CURVE_ROLE(),
+                address(csm)
+            ),
+            "reset bond curve csm"
+        );
+        assertEq(
+            accounting.getRoleMemberCount(accounting.RECOVERER_ROLE()),
+            0,
+            "recoverer"
+        );
+    }
 }
 
 contract CSFeeDistributorInvariants is InvariantsBase {
@@ -196,5 +301,50 @@ contract CSFeeDistributorInvariants is InvariantsBase {
                 "tree exists, but has no CID"
             );
         }
+    }
+
+    function test_roles() public {
+        assertEq(
+            feeDistributor.getRoleMemberCount(
+                feeDistributor.DEFAULT_ADMIN_ROLE()
+            ),
+            1
+        );
+        assertEq(
+            feeDistributor.getRoleMemberCount(feeDistributor.RECOVERER_ROLE()),
+            0
+        );
+    }
+}
+
+contract CSFeeOracleInvariant is InvariantsBase {
+    function test_roles() public {
+        assertEq(
+            oracle.getRoleMemberCount(oracle.DEFAULT_ADMIN_ROLE()),
+            1,
+            "default admin"
+        );
+        assertEq(
+            oracle.getRoleMemberCount(oracle.CONTRACT_MANAGER_ROLE()),
+            0,
+            "contract manager"
+        );
+        assertEq(
+            oracle.getRoleMemberCount(oracle.SUBMIT_DATA_ROLE()),
+            0,
+            "submit data"
+        );
+        assertEq(oracle.getRoleMemberCount(oracle.PAUSE_ROLE()), 1, "pause");
+        assertEq(
+            csm.getRoleMember(oracle.PAUSE_ROLE(), 0),
+            address(gateSeal),
+            "pause address"
+        );
+        assertEq(oracle.getRoleMemberCount(oracle.RESUME_ROLE()), 0, "resume");
+        assertEq(
+            oracle.getRoleMemberCount(oracle.RECOVERER_ROLE()),
+            0,
+            "recoverer"
+        );
     }
 }
