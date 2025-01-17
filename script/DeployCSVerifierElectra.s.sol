ad3e06570ff4f8343cb48037f542c11e4927dfa1
madlabman <10616301+madlabman@users.noreply.github.com>
2025-01-17 12:33:18 +0100

    chore: set pivotSlot to unreachabe one

--- script/DeployCSVerifierElectra.s.sol
+++ script/DeployCSVerifierElectra.s.sol
@@ -87,7 +87,7 @@ contract DeployCSVerifierHolesky is DeployCSVerifier {
             gIHistoricalSummariesPrev: HISTORICAL_SUMMARIES_DENEB,
             gIHistoricalSummariesCurr: HISTORICAL_SUMMARIES_ELECTRA,
             firstSupportedSlot: Slot.wrap(950272), // 269_568 * 32, @see https://github.com/eth-clients/mainnet/blob/main/metadata/config.yaml#L52
-            pivotSlot: Slot.wrap(0) // TODO: Update with Electra slot.
+            pivotSlot: Slot.wrap(type(uint64).max) // TODO: Update with Electra slot.
         });
     }
 }
@@ -105,7 +105,7 @@ contract DeployCSVerifierMainnet is DeployCSVerifier {
             gIHistoricalSummariesPrev: HISTORICAL_SUMMARIES_DENEB,
             gIHistoricalSummariesCurr: HISTORICAL_SUMMARIES_ELECTRA,
             firstSupportedSlot: Slot.wrap(8626176), // 29_696 * 32, @see https://github.com/eth-clients/holesky/blob/main/metadata/config.yaml#L38
-            pivotSlot: Slot.wrap(0) // TODO: Update with Electra slot.
+            pivotSlot: Slot.wrap(type(uint64).max) // TODO: Update with Electra slot.
         });
     }
 }
