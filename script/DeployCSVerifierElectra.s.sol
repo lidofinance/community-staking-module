f11ad25797c0452b6a71278ebde45886e332ccd7
madlabman <10616301+madlabman@users.noreply.github.com>
2025-01-17 12:31:31 +0100

    fix: broadcast transaction

--- script/DeployCSVerifierElectra.s.sol
+++ script/DeployCSVerifierElectra.s.sol
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2024 Lido <info@lido.fi>
 // SPDX-License-Identifier: GPL-3.0
 
-// Usage: forge script ./script/DeployCSVerifierElectra.s.sol:DeployCSVerifier[Holesky|Mainnet]
+// Usage: forge script --private-key=$PRIVATE_KEY ./script/DeployCSVerifierElectra.s.sol:DeployCSVerifier[Holesky|Mainnet]
 
 pragma solidity 0.8.24;
 
@@ -50,23 +50,28 @@ GIndex constant FIRST_VALIDATOR_ELECTRA = GIndex.wrap(
 );
 
 abstract contract DeployCSVerifier is Script {
+    CSVerifier internal verifier;
     Config internal config;
 
     function run() public {
-        CSVerifier verifier = new CSVerifier({
-            withdrawalAddress: config.withdrawalVault,
-            module: config.module,
-            slotsPerEpoch: config.slotsPerEpoch,
-            gIFirstWithdrawalPrev: config.gIFirstWithdrawalPrev,
-            gIFirstWithdrawalCurr: config.gIFirstWithdrawalCurr,
-            gIFirstValidatorPrev: config.gIFirstValidatorPrev,
-            gIFirstValidatorCurr: config.gIFirstValidatorCurr,
-            gIHistoricalSummariesPrev: config.gIHistoricalSummariesPrev,
-            gIHistoricalSummariesCurr: config.gIHistoricalSummariesCurr,
-            firstSupportedSlot: config.firstSupportedSlot,
-            pivotSlot: config.pivotSlot,
-            admin: config.admin
-        });
+        vm.startBroadcast();
+        {
+            verifier = new CSVerifier({
+                withdrawalAddress: config.withdrawalVault,
+                module: config.module,
+                slotsPerEpoch: config.slotsPerEpoch,
+                gIFirstWithdrawalPrev: config.gIFirstWithdrawalPrev,
+                gIFirstWithdrawalCurr: config.gIFirstWithdrawalCurr,
+                gIFirstValidatorPrev: config.gIFirstValidatorPrev,
+                gIFirstValidatorCurr: config.gIFirstValidatorCurr,
+                gIHistoricalSummariesPrev: config.gIHistoricalSummariesPrev,
+                gIHistoricalSummariesCurr: config.gIHistoricalSummariesCurr,
+                firstSupportedSlot: config.firstSupportedSlot,
+                pivotSlot: config.pivotSlot,
+                admin: config.admin
+            });
+        }
+        vm.stopBroadcast();
         console.log("CSVerifier deployed at:", address(verifier));
     }
 }
