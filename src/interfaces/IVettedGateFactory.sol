2f31d56a740893b98c9920c657fee6ca7fa0c7c2
skhomuti <skhomuti@gmail.com>
2025-02-17 19:33:06 +0500

    add VETTED_GATE_IMPL to the interface

--- src/interfaces/IVettedGateFactory.sol
+++ src/interfaces/IVettedGateFactory.sol
@@ -8,6 +8,10 @@ interface IVettedGateFactory {
 
     error ZeroImplementationAddress();
 
+    /// @dev address of the VettedGate implementation to be used for the new instances
+    /// @return address of the VettedGate implementation
+    function VETTED_GATE_IMPL() external view returns (address);
+
     /// @dev Creates a new VettedGate instance behind the OssifiableProxy based on known implementation address
     /// @param curveId Id of the bond curve to be assigned for the eligible members
     /// @param treeRoot Root of the eligible members Merkle Tree
