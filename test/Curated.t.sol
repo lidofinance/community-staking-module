6c084881d175571d3306bb020d5536ae5efc4e16
madlabman <10616301+madlabman@users.noreply.github.com>
2025-04-15 14:28:29 +0200

    test: add more tests for Curated

--- test/Curated.t.sol
+++ test/Curated.t.sol
@@ -12,6 +12,9 @@ import { Stub } from "./helpers/mocks/Stub.sol";
 import { CSMFixtures } from "./CSModule.t.sol";
 
 contract CuratedTest is CSMFixtures {
+    uint64 internal constant PUBKEY_LENGTH = 48;
+    uint64 internal constant SIGNATURE_LENGTH = 96;
+
     function setUp() public virtual {
         nodeOperator = nextAddress("NODE_OPERATOR");
         stranger = nextAddress("STRANGER");
@@ -92,7 +95,10 @@ contract CuratedTest is CSMFixtures {
         csm.obtainDepositData(40, "");
     }
 
-    function test_obtainDepositData_UsesTheLeastActiveKeysFirst() public {
+    function test_obtainDepositData_UsesTheLeastActiveKeysFirst()
+        public
+        assertInvariants
+    {
         createNodeOperator(100);
         createNodeOperator(100);
 
@@ -105,11 +111,7 @@ contract CuratedTest is CSMFixtures {
         }
         csm.obtainDepositData(100, "");
 
-        csm.updateExitedValidatorsCount(
-            bytes.concat(bytes8(uint64(1))),
-            bytes.concat(bytes16(uint128(18)))
-        ); // -> NO{id: 1, active: 32}
-
+        setExited({ noId: 1, to: 18 }); // -> NO{id: 1, active: 32}
         {
             vm.expectEmit(address(csm));
             emit ICSModule.DepositedSigningKeysCountChanged(0, 50 + 1);
@@ -118,5 +120,112 @@ contract CuratedTest is CSMFixtures {
             emit ICSModule.DepositedSigningKeysCountChanged(1, 50 + 18 + 1);
         }
         csm.obtainDepositData(20, "");
+    }
+
+    function test_obtainDepositData_TakesIntoAccountDepositable()
+        public
+        assertInvariants
+    {
+        createNodeOperator(100);
+        createNodeOperator(100);
+
+        unvetKeys({ noId: 1, to: 30 });
+        {
+            vm.expectEmit(address(csm));
+            emit ICSModule.DepositedSigningKeysCountChanged(0, 70);
+
+            vm.expectEmit(address(csm));
+            emit ICSModule.DepositedSigningKeysCountChanged(1, 30);
+        }
+        csm.obtainDepositData(100, "");
+    }
+
+    function test_obtainDepositData_SkipsNonDepositable()
+        public
+        assertInvariants
+    {
+        createNodeOperator(0);
+        createNodeOperator(1);
+        createNodeOperator(1);
+
+        unvetKeys({ noId: 1, to: 0 });
+        {
+            vm.expectEmit(address(csm));
+            emit ICSModule.DepositedSigningKeysCountChanged(2, 1);
+        }
+        csm.obtainDepositData(1, "");
+    }
+
+    function test_obtainDepositData_ReturnsCorrectDepositData()
+        public
+        assertInvariants
+    {
+        (bytes memory keys, bytes memory signatures) = keysSignatures(15);
+
+        createNodeOperator(
+            nodeOperator,
+            5,
+            slice(keys, PUBKEY_LENGTH * 0, PUBKEY_LENGTH * 5),
+            slice(signatures, SIGNATURE_LENGTH * 0, SIGNATURE_LENGTH * 5)
+        );
+        createNodeOperator(
+            nodeOperator,
+            5,
+            slice(keys, PUBKEY_LENGTH * 5, PUBKEY_LENGTH * 5),
+            slice(signatures, SIGNATURE_LENGTH * 5, SIGNATURE_LENGTH * 5)
+        );
+        createNodeOperator(
+            nodeOperator,
+            5,
+            slice(keys, PUBKEY_LENGTH * 10, PUBKEY_LENGTH * 5),
+            slice(signatures, SIGNATURE_LENGTH * 10, SIGNATURE_LENGTH * 5)
+        );
+
+        (keys, signatures) = csm.obtainDepositData(3, "");
+        // prettier-ignore
+        {
+            assertEq(keys.length, PUBKEY_LENGTH * 3);
+            assertEq(keys[PUBKEY_LENGTH * 1 - 1], bytes1(uint8(1)), "Unexpected key from noId=0");
+            assertEq(keys[PUBKEY_LENGTH * 2 - 1], bytes1(uint8(6)), "Unexpected key from noId=1");
+            assertEq(keys[PUBKEY_LENGTH * 3 - 1], bytes1(uint8(11)), "Unexpected key from noId=2");
+        }
+
+        (keys, signatures) = csm.obtainDepositData(1, "");
+        // prettier-ignore
+        {
+            assertEq(keys.length, PUBKEY_LENGTH * 1);
+            assertEq(keys[PUBKEY_LENGTH * 1 - 1], bytes1(uint8(2)), "Unexpected key from noId=0");
+        }
+
+        (keys, signatures) = csm.obtainDepositData(4, "");
+        // prettier-ignore
+        {
+            assertEq(keys.length, PUBKEY_LENGTH * 4);
+            assertEq(keys[PUBKEY_LENGTH * 1 - 1], bytes1(uint8(3)), "Unexpected key from noId=0");
+            assertEq(keys[PUBKEY_LENGTH * 2 - 1], bytes1(uint8(7)), "Unexpected key from noId=1");
+            assertEq(keys[PUBKEY_LENGTH * 3 - 1], bytes1(uint8(8)), "Unexpected key from noId=1");
+            assertEq(keys[PUBKEY_LENGTH * 4 - 1], bytes1(uint8(12)), "Unexpected key from noId=2");
+        }
+    }
+
+    function test_obtainDepositData_ZeroCountOfDeposits()
+        public
+        assertInvariants
+    {
+        createNodeOperator(20);
+
+        (bytes memory publicKeys, bytes memory signatures) = csm
+            .obtainDepositData(0, "");
+
+        assertEq(publicKeys.length, 0, "Expected no keys returned");
+        assertEq(signatures.length, 0, "Expected no signatures returned");
+    }
+
+    function test_obtainDepositData_RevertsIfNotEnoughKeys() public {
+        createNodeOperator(20);
+        createNodeOperator(0);
+
+        vm.expectRevert(ICSModule.NotEnoughKeys.selector);
+        csm.obtainDepositData(21, "");
     }
 }
